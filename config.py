915955fa1c45c9aed5e554b2597f60ac6bde6539
Sakula <mohittatu2007@gmail.com>
2025-08-10 13:46:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,28 +4,28 @@ class Config(object):
     # Telegram Bot ka token
     BOT_TOKEN = "7691272055:AAEb2cq9_G3JKWrpq5NKSlXI6wEF1CLj4tc"
     # Telegram API ki ID
-    API_ID = 27660379
-    API_HASH = "19c71c27733f0954371085198855125a"
-    ADMIN_ID = [5459854363]
+    API_ID = 19822115
+    API_HASH = "d251f606bc7910135770ac2d454dd2c6"
+    ADMIN_ID = [7793257011]
     # MongoDB database ka URL
     DB_URL = "mongodb+srv://sindhavravi11:%24Sind1234567890@cluster0.bwlgbx3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     # Database ka naam
     DB_NAME = "MY_BOT_DB"
     # Text log channel ki ID
-    TXT_LOG = -1002761572365
+    TXT_LOG = -1002652695292
     # Authentication log channel ki ID
-    AUTH_LOG = -1002761572365
+    AUTH_LOG = -1002793906910
     # Hit log channel ki ID
-    HIT_LOG = -1002761572365
+    HIT_LOG = -1002652695292
     # DRM dump channel ki ID
-    DRM_DUMP = -1002761572365
+    DRM_DUMP = -1002652695292
     # Main channel ki ID
-    CHANNEL = -1002761572365
+    CHANNEL = -1002652695292
     # Channel ka link
-    CH_URL = "https://t.me/ravi1996sindhav"
+    CH_URL = "https://t.me/Mrfrontman001"
     # Bot ke owner ka Telegram link
     OWNER = "https://t.me/username"
     # Thumbnail image ka URL
-    THUMB_URL = "https://telegra.ph/file/example-thumb-image.jpg" #Replace by with your Thumb URL
-    PREMIUM_LOGS = "-1002761572365"
+    THUMB_URL = "https://files.catbox.moe/ozmb83.jpg" #Replace by with your Thumb URL
+    PREMIUM_LOGS = "-1002652695292"
     
